31d596da3f9cccf01a1a29eed30415d649a8b28f
natspindola <natspindola@hotmail.com>
2022-02-25 18:53:20 -0300

    ComponentModel pt.2

--- CSharp - ASP.NET MVC/Models/Pessoa.cs
+++ CSharp - ASP.NET MVC/Models/Pessoa.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,19 @@ namespace CSharp___ASP.NET_MVC.Models
 {
     public class Pessoa
     {
+        [Required(ErrorMessage = "O nome é obrigatório ")]
         [DisplayName("Nome da pessoa:")]
         public string Nome { get; set; }
+
         [DisplayName("Idade da pessoa:")]
         public  int idade { get; set; }
+
+        [Required(ErrorMessage = "O endereço é obrigatório ")]
         [DisplayName("Endereço da pessoa:")]
         public string Endereco { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório ")]
+        [DisplayName("Email da pessoa:")]
+        public string Email { get; set; }
     }
 }

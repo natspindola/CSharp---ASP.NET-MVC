978bbbea4780e8d52a0ba8ec03f5de24ecb61dee
natspindola <natspindola@hotmail.com>
2022-02-25 18:32:46 -0300

    ComponentModel pt.1

--- CSharp - ASP.NET MVC/Models/Pessoa.cs
+++ CSharp - ASP.NET MVC/Models/Pessoa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,11 @@ namespace CSharp___ASP.NET_MVC.Models
 {
     public class Pessoa
     {
+        [DisplayName("Nome da pessoa:")]
         public string Nome { get; set; }
+        [DisplayName("Idade da pessoa:")]
         public  int idade { get; set; }
+        [DisplayName("Endereço da pessoa:")]
         public string Endereco { get; set; }
     }
 }

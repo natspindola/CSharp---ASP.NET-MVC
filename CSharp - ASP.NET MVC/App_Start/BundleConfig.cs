ceaa73a17d8556f909458fb3efacd22f34893a8b
natspindola <natspindola@hotmail.com>
2022-02-28 09:30:17 -0300

    bundle e minification

--- CSharp - ASP.NET MVC/App_Start/BundleConfig.cs
+++ CSharp - ASP.NET MVC/App_Start/BundleConfig.cs
@@ -20,11 +20,13 @@ namespace CSharp___ASP.NET_MVC
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
